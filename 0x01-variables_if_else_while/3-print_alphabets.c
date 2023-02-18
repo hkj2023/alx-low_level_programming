24f63528296c87409a9f8a10f5a3c7221adb3dce
desu6 <mail@y.cm>
2023-02-17 23:05:16 -0800

    My commit on task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry to display ALphabet.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 'A'; n <= 'Z'; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
