2cff8645f49d3f831e92906a0a8b1f43d29d87ba
desu6 <mail@y.cm>
2023-02-17 22:57:20 -0800

    My commit on task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry will display char in lower case
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
