4c0ff53559ea8353c30f477ca7b2755d29b024e9
desu6 <mail@y.cm>
2023-02-17 22:34:33 -0800

    My last commit on task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,31 +3,31 @@
 #include <stdio.h>
 
 /**
- * main - Entry is to tell the length.
- * Return: Always 0 (Success)
+ *main - Entry is to tell us the length
+ *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-        int n, lastdgt;
+	int n, lastdgt;
 
-        srand(time(0));
+	srand(time(0));
 
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
-
+	n = rand() - RAND_MAX / 2;
+	/* Here is my cose */
 	lastdgt = n % 10;
 
-        if (lastdgt > 5){
-                printf("Last digit of %d is %d and is greater than 5\n", n, lastdgt);
-	        }
-        else if (lastdgt == 0){
-                printf("Last digit of %d is %d and 0\n", n, lastdgt);
-	        }
-        else if (lastdgt < 6 && lastdgt !=0){
-		printf("Last digit of %d is %d and is less 6 and not 0\n", n, lastdgt);
-	        }  
-        return (0);
-
+	if (lastdgt > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastdgt);
+	}
+	else if (lastdgt == 0)
+	{
+	printf("Last digit of %d is %d and 0\n", n, lastdgt);
+	}
+	else if (lastdgt < 6 && lastdgt != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdgt);
+	}
+	return (0);
 }
-
