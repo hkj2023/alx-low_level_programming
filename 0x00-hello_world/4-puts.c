c4622917e4b8e3b184973e78d02f773c01630144
desu6 <mail@y.cm>
2023-02-19 22:03:52 -0800

    Task 4 is done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building ia multilingual puzzle");
+        return (0);
+}
