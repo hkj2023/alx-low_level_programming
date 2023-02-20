980428cc8b554a61c71f8b272169241091f02ddc
desu6 <mail@y.cm>
2023-02-20 04:26:12 -0800

    Task 8 is done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+return (1);
+}
