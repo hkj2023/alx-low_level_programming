35d0129d7c5d4e9b5511ec0f5676bfe20cb27bbc
desu6 <mail@y.cm>
2023-02-21 10:08:48 -0800

    Task 1 is done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point.
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');;
+}
+
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main()
+{
+print_alphabet();
+return (0);
+}
