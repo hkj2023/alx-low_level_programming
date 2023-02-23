f6d131aaf33199602e4869a03f50f205cbc8dfb0
desu6 <mail@y.cm>
2023-02-23 08:37:53 -0800

    Task 2 is done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul -  to print multiplication of two number
+ * @x: first number
+ * @y: second number
+ * Return: Always 0 (Success).
+ */
+int mul(int x, int y)
+{
+
+	int prod;
+
+	prod = x * y;
+
+	return (prod);
+}
