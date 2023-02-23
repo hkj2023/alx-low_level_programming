72dd9348020f4135d64eb3231472356908caddad
desu6 <mail@y.cm>
2023-02-23 01:13:03 -0800

    Task 8 is doneee!

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,8 +3,8 @@
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
- * Return: nothing
  */
+
 void jack_bauer(void)
 {
 int x, y;
@@ -13,7 +13,7 @@ while (x < 24)
 {
 y = 0;
 while (y < 60)
-	{
+{
 _putchar((x / 10) + '0');
 _putchar((x % 10) + '0');
 _putchar(':');
@@ -22,6 +22,6 @@ _putchar((y % 10) + '0');
 _putchar('\n');
 y++;
 }
-}
 x++;
+}
 }
