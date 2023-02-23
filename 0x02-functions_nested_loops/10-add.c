15b1cb37aded8b4b12c9ee14f04769b67ab3a638
desu6 <mail@y.cm>
2023-02-23 03:06:31 -0800

    Task 10 is over

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @x: int to be added to y
+ * @y: int to be added to x
+ * Return: sum of two numbersb
+ */
+
+int add(int x, int y)
+{
+int sum;
+sum = x + y;
+return (sum);
+}
