08ec50a54c2d16d395224a166b80a4378fba8d87
desu6 <mail@y.cm>
2023-02-24 08:11:46 -0800

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,20 +4,21 @@
  * print_line - print line
  * * Return: Always 0 (SUCCESS)
  */
+
 void print_line(int n)
 {
-	int j;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
-		for (j = 1; j <= n; j++)
+		int i;
+
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
 	}
+
 }
