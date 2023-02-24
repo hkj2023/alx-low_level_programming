6534c034ba505ca4de79f710960e4c6d06069520
desu6 <mail@y.cm>
2023-02-24 10:45:54 -0800

    task 0

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This file contains readme file about 0x03-debugging
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+postitive_or_negative();
+print_remaining_days();
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+
+#endif
