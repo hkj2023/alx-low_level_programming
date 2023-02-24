1e2d627105ccb20ce08e28e9961c87fdb83a64ff
desu6 <mail@y.cm>
2023-02-24 04:54:35 -0800

    Task 5 is done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - more number function.rom 0 to 14
+ * Return: Always 0 (Successful)
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
