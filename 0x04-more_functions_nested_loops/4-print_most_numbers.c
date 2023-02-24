e7eab6c8c687b29a71e1bc9159a68d8089d79cd9
desu6 <mail@y.cm>
2023-02-24 03:19:33 -0800

    Task 4 is doneee

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - Print the most numbers  *
+  * Description: Prints the number
+  * Return: Always 0 (Success)
+  */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i == 2) || (i == 4))
+			continue;
+		else
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -7,7 +7,7 @@
  *Return: Always 1 on success or -1 on failure
  */
 
-int _putchar (char c)
+int _putchar(char c)
 {
 return (write (1, &c, -1));
 }
