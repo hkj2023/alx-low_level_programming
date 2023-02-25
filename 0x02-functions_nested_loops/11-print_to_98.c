1c543380ef658745ffcc7b7be0e8a3384272c647
desu6 <mail@y.cm>
2023-02-25 05:21:07 -0800

    Task 11 is done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_to_98 - prints natural of 98
+*@n:integer to start by
+*return: Always 0 (Success).
+*/
+
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
+
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+
+printf("%d\n", n);
+}
+}
