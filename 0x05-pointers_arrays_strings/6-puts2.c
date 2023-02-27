b432be7e5b042da3e49612e25a80828f9434cf1e
desu6 <mail@y.cm>
2023-02-27 10:18:28 -0800

    After betty

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,14 @@
  */
 void puts2(char *str)
 {
-	int lngth = 0, a;
-	while (str[lngth] != '\0')
-	{
-		lngth++;
-	}
-	for (a = 0; a < lngth; a = a + 2)
-	{
-		_putchar(str[a]);
-	}
-
-	_putchar('\n');
+int lngth = 0, a;
+while (str[lngth] != '\0')
+{
+lngth++;
+}
+for (a = 0; a < lngth; a = a + 2)
+{
+_putchar(str[a]);
+}
+_putchar('\n');
 }
