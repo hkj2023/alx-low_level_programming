d43b004269465058d369ce6489ec3dfe3298c41d
desu6 <mail@y.cm>
2023-02-27 10:38:27 -0800

    Task 8 is done

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print arrays element
+ * @n array of integer
+ * @c array of pointer
+ * @n: int type integer
+ * Return Alwys 0 (Success).
+ */
+
+void print_array(int *c, int n)
+{
+int i = 0;
+for (n--; n >= 0; n--, i++)
+{
+printf("%d", c[i]);
+if (n > 0)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
