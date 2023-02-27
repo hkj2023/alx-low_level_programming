da4f08a0aea0a5bdd082f8df659fc4d987301795
desu6 <mail@y.cm>
2023-02-27 10:14:37 -0800

    Task 6 is done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints char out of 2.
+ * lngth the lenght of array
+ * @str: string
+ * @a intiger
+ */
+void puts2(char *str)
+{
+	int lngth = 0, a;
+	while (str[lngth] != '\0')
+	{
+		lngth++;
+	}
+	for (a = 0; a < lngth; a = a + 2)
+	{
+		_putchar(str[a]);
+	}
+
+	_putchar('\n');
+}
