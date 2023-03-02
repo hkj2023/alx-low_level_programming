04a47fcdfde45df4fdd138a1e80b8656a24f5daf
desu6 <mail@y.cm>
2023-03-02 10:12:27 -0800

    Task 3 is done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - to compare string pointers.
+ * @s1: string 1.
+ * @s2: string 2.
+ * Return: Always 0 (Success).
+ */
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 && *s2)
+{
+if (*s1 == *s2)
+s1++;
+s2++;
+}
+return (*s1 - *s2);
+}
