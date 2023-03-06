78ccb8eb770ec98b7b7d65ab0874c41e8ed23474
desu6 <mail@y.cm>
2023-03-06 05:47:31 -0800

    Task 4 is done

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strpbrk - to search somethinges.
+ * @s: the string
+ * @accept: bytes
+ * Return: Always 0 (Success).
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+return (s);
+}
+s++;
+}
+return ('\0');
+}
