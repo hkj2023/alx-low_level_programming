79628d445663fb743c355309fcdf5b10e32b9be9
desu6 <mail@y.cm>
2023-03-06 07:02:53 -0800

    Task 7 is done

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - to print chessboard
+ * @a: the chessboard
+ * Return: Always 0 (Success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int x, y;
+for (x = 0; x < 8; x++)
+{
+for (y = 0; y < 8; y++)
+{
+_putchar(a[x][y]);
+}
+_putchar('\n');
+}
+}
