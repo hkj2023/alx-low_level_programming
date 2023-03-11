8d95e89fbdbc19f13665ab7bdfd390ac9f52c141
desu6 <mail@y.cm>
2023-03-11 00:00:38 -0800

    Task 0 is updated at the end

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -7,8 +8,9 @@
  * Return: Always 0 (Success)
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
+(void) argc;
 printf("%s\n", argv[0]);
 return (0);
 }
