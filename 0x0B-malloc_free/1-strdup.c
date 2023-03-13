4861e592272a43ace8928d10c78e5a46c02c5b46
desu6 <mail@y.cm>
2023-03-13 05:40:59 -0700

    Task 1 is done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strdup - to duplicate the string;
+ * @str: -the dtring chacter
+ * str - the string
+ * main: entry point.
+ * Return: Always 0 (Success)
+ */
+char *_strdup(char *str)
+{
+char *strdup;
+int x, y;
+if (str == NULL)
+return (NULL);
+for (x = 0; str[x] != '\0'; x++)
+strdup = (char *)malloc(sizeof(char) * (x + 1));
+if (strdup == NULL)
+return (NULL);
+for (y = 0; y <= x; y++)
+strdup[y] = str[y];
+return (strdup);
+}
