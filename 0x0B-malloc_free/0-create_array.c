2d8f815b1a135236505fca77a4722dbc5c6a048a
desu6 <mail@y.cm>
2023-03-13 03:51:38 -0700

    Task 0 is done again

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,9 @@ unsigned int i;
 if (size == 0)
 return (NULL);
 a = malloc(size * sizeof(char));
+if (a == NULL)
+return (NULL);
 for (i = 0 ; i < size; i++)
 a[i] = c;
 return (a);
-if (a == NULL)
-return (NULL);
 }
