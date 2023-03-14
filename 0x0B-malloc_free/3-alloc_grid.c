ee0b3879b00dca5f1a96489de5d72a4bb0e69c07
desu6 <mail@y.cm>
2023-03-14 02:50:49 -0700

    Task 3 is done again

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **grid;
 int w, h;
 if (width <= 0 || height <= 0)
 return (NULL);
-grid = malloc(sizeof(int*) * height);
+grid = malloc(sizeof(int *) * height);
 if (grid == NULL)
 return (NULL);
 for (h = 0; h < height; h++)
