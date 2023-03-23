d9fa0fd8b4cc8b938e03ac97dcd97a5bbc4cca9c
desu6 <mail@y.cm>
2023-03-23 05:23:27 -0700

    Task 1 is updated

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,8 +12,9 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-unsigned int i = 0;
 va_list apt;
+unsigned int i;
+i = 0;
 va_start(apt, n);
 for (i = 0; i < n; i++)
 {
