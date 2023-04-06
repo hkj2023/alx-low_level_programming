23475dda965ab6aff07a1aba02817976114f5d37
desu6 <mail@y.cm>
2023-04-06 09:13:35 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,12 +8,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int result;
-int i,j;
-i = 0, result = 0;
+int i, j;
+i = 0;
+result = 0;
 if (b[i] == '\0')
 return (0);
-for (j=0;j<=i;j++)
-if((b[i] == '0' || b[i] == '1'))  {
+for (j = 0; j <= i; j++)
+if ((b[i] == '0' || b[i] == '1'))
+{
 result <<= 1;
 result += b[i] - '0';
 i++;
