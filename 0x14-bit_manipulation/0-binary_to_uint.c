07d0cd6040fe980a258fb11ef47c298b1b983c59
desu6 <mail@y.cm>
2023-04-06 09:40:27 -0700

    Bitwise 0 task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,17 +5,18 @@
  * @b: character
  * Return: Always 0 (Success)
  */
+
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int result = 0;
 int i = 0;
-if (b[i] == '\0')
+if (!b)
 return (0);
-if ((b[i] == '0') || (b[i] == '1'))
+for (; b[i]; i++)
 {
-result <<= 1;
-result += b[i] - '0';
-i++;
+if ((b[i] < '0') || (b[i] > '1'))
+return (0);
+result = 2 * result + (b[i] - '0');
 }
 return (result);
 }
