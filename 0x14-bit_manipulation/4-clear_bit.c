741178c0c95e0b084504863c7942350769a2403b
desu6 <mail@y.cm>
2023-04-07 02:10:04 -0700

    task 4 is done again

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int number;
-	if (index > sizeof(n) * 8)
+if (index > sizeof(n) * 8)
 return (-1);
 number = ~(1 << index);
 *n &= number;
