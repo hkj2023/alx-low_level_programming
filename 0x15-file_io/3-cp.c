ca199f92ec09f7b99a519e5702eac4fcd8fea872
desu6 <mail@y.cm>
2023-04-12 10:45:05 -0700

    Task 4 is updated finally

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,36 +10,41 @@
  */
 int main(int argc, char *argv[])
 {
-int file_from, file_to;
-ssize_t number;
-char buffer[BUFFER];
+int file_from, file_to, a, b, c;
+char bufer[BUFFER];
 if (argc != 3)
-dprintf(STDERR_FILENO, "Usage: copy file_from file_to\n");
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
+}
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
+{
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
-file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (file_to == -1)
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-while ((number = read(file_from, buffer, BUFFER)) > 0)
+}
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+while ((a = read(file_from, bufer, BUFFER)) > 0)
 {
-if (write(file_to, buffer, number) != number)
+if (file_to == -1 || write(file_to, bufer, a) != a)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(file_from);
 exit(99);
 }
 }
-if (number == -1)
+if (a == -1)
+{
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
-if (close(file_from) != 0)
+}
+b = close(file_from);
+c = close(file_to);
+if (b == -1 || c == -1)
+{
+if (b == -1)
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
-if (close(file_to) != 0)
-{
+if (c == -1)
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 exit(100);
 }
